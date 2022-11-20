f283ba0ef8aa084497e0092b90c29b085354973b
Nasim-Alizai <sayedhakimi449@gmail.com>
2022-11-20 10:43:35 +0430

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import 'quiz.dart';
+import 'result.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'what is your favorate color ?',
       'answers': ['Black', 'Red', 'Blue', 'green'],
@@ -48,19 +48,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text("my first flutter"),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: <Widget>[
-                  Question(questions[_questionIndex]['questionText'] as String),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList(),
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
               )
-            : const Center(
-                child: Text("you did it"),
-              ),
+            : Result(),
       ),
     );
   }
---
+++ lib/quiz.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final VoidCallback answerQuestion;
+  final int questionIndex;
+
+  Quiz({
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(questions[questionIndex]['questionText'] as String),
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(answerQuestion, answer);
+        }).toList(),
+      ],
+    );
+  }
+}
